2edcd3c740402f655bea9fd28617d5689f302798
Ze'ev Klapow <zklapow@hubspot.com>
2015-07-21 13:18:36 -0400

    client: send is non-blocking, commands are queued.
    This is important because it allows a listener on a future from a send
    response to execute another send, with deadlocking.

--- src/main/java/com/hubspot/imap/imap/command/CommandType.java
+++ src/main/java/com/hubspot/imap/imap/command/CommandType.java
@@ -9,5 +9,6 @@ public enum CommandType {
   LIST,
   EXAMINE,
   SELECT,
-  FETCH;
+  FETCH,
+  BLANK;
 }
---
+++ src/test/java/com/hubspot/imap/ImapClientAuthenticationTest.java
@@ -0,0 +1,22 @@
+package com.hubspot.imap;
+
+import com.hubspot.imap.client.ImapClient;
+import com.hubspot.imap.imap.exceptions.AuthenticationFailedException;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ImapClientAuthenticationTest {
+
+  @Test
+  public void testGivenInvalidCredentials_doesThrowAuthenticationException() throws Exception {
+    try (ImapClient client = TestUtils.CLIENT_FACTORY.connect(TestUtils.USER_NAME, "")) {
+      client.login();
+      client.awaitLogin();
+    } catch (ExecutionException e) {
+      assertThat(e).hasCauseInstanceOf(AuthenticationFailedException.class);
+    }
+  }
+}

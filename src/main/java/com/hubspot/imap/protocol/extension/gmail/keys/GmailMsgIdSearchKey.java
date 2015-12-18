ddb8006e8f41b849c08bfc77f7358bede1384b78
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2015-12-18 13:23:54 -0500

    Add keyword search key + gmail msg id search key

---
+++ src/main/java/com/hubspot/imap/protocol/extension/gmail/keys/GmailMsgIdSearchKey.java
@@ -0,0 +1,10 @@
+package com.hubspot.imap.protocol.extension.gmail.keys;
+
+import com.hubspot.imap.protocol.command.search.keys.BaseSearchKey;
+import com.hubspot.imap.protocol.extension.gmail.GmailSearchKeyTypes;
+
+public class GmailMsgIdSearchKey extends BaseSearchKey {
+  public GmailMsgIdSearchKey(long msgId) {
+    super(GmailSearchKeyTypes.MSGID, String.valueOf(msgId));
+  }
+}

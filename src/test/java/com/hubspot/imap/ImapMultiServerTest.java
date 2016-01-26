5a0389fe20e4f1294888af886b2885986e4df4d3
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2016-01-26 09:49:44 -0500

    Add outlook 365 profile

--- src/test/java/com/hubspot/imap/ImapMultiServerTest.java
+++ src/test/java/com/hubspot/imap/ImapMultiServerTest.java
@@ -2,6 +2,7 @@ package com.hubspot.imap;
 
 import com.hubspot.imap.profiles.EmailServerTestProfile;
 import com.hubspot.imap.profiles.GmailProfile;
+import com.hubspot.imap.profiles.Outlook365Profile;
 import com.hubspot.imap.profiles.OutlookProfile;
 import java.util.Arrays;
 import java.util.Collection;
@@ -11,7 +12,8 @@ import org.junit.runners.Parameterized.Parameters;
 public abstract class ImapMultiServerTest {
   protected static final List<EmailServerTestProfile> TEST_PROFILES = Arrays.asList(
     GmailProfile.getGmailProfile(),
-    OutlookProfile.getOutlookProfile()
+    OutlookProfile.getOutlookProfile(),
+    Outlook365Profile.getOutlook365Profile()
   );
 
   @Parameters(name="{0}")

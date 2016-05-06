fd3ad7a31edcf554756895a01d4cb707e639815a
Matthew Singer <msinger@hubspot.com>
2016-05-06 16:12:12 -0400

    Test all the labels

--- src/test/java/com/hubspot/imap/protocol/extension/gmail/GMailLabelTest.java
+++ src/test/java/com/hubspot/imap/protocol/extension/gmail/GMailLabelTest.java
@@ -1,5 +1,6 @@
 package com.hubspot.imap.protocol.extension.gmail;
 
+import com.google.common.collect.Sets;
 import com.hubspot.imap.client.FolderOpenMode;
 import com.hubspot.imap.client.ImapClient;
 import com.hubspot.imap.profiles.GmailProfile;
@@ -10,38 +11,20 @@ import com.hubspot.imap.protocol.response.ResponseCode;
 import com.hubspot.imap.protocol.response.tagged.FetchResponse;
 import com.hubspot.imap.protocol.response.tagged.OpenResponse;
 import io.netty.util.concurrent.Future;
-import org.assertj.core.api.Condition;
 import org.junit.Test;
 
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GMailLabelTest {
   private static final GmailProfile GMAIL_PROFILE = GmailProfile.getGmailProfile();
-  @Test
-  public void testCanFetchGMailLabels() throws Exception {
-    try (ImapClient client = GMAIL_PROFILE.getLoggedInClient()) {
-      Future<OpenResponse> openResponseFuture = client.open(GMAIL_PROFILE.getImplDetails().getAllMailFolderName(), FolderOpenMode.WRITE);
-      OpenResponse or = openResponseFuture.get();
-      assertThat(or.getCode()).isEqualTo(ResponseCode.OK);
-
-      Future<FetchResponse> fetchResponseFuture = client.fetch(1, Optional.of(2L), FetchDataItemType.X_GM_LABELS);
-      FetchResponse fetchResponse = fetchResponseFuture.get();
-
-      assertThat(fetchResponse.getMessages().size()).isGreaterThan(0);
-      assertThat(fetchResponse.getMessages()).have(new Condition<>(m -> {
-        try {
-          return m.getGMailLabels() != null;
-        } catch (UnfetchedFieldException e) {
-          return false;
-        }
-      }, "gmail labels"));
-    }
-  }
 
   @Test
-  public void testCanFetchDraftLabels() throws Exception {
+  public void testCanFetchLabels() throws Exception {
+    Set<SystemLabel> systemLabels = Sets.immutableEnumSet(SystemLabel.DRAFTS, SystemLabel.INBOX, SystemLabel.SENT);
+
     try (ImapClient client = GMAIL_PROFILE.getLoggedInClient()) {
       Future<OpenResponse> openResponseFuture = client.open(GMAIL_PROFILE.getImplDetails().getAllMailFolderName(), FolderOpenMode.WRITE);
       OpenResponse or = openResponseFuture.get();
@@ -50,14 +33,19 @@ public class GMailLabelTest {
       Future<FetchResponse> fetchResponseFuture = client.fetch(1, Optional.empty(), FetchDataItemType.X_GM_LABELS);
       FetchResponse fetchResponse = fetchResponseFuture.get();
 
-      assertThat(fetchResponse.getMessages().size()).isGreaterThan(0);
-      assertThat(fetchResponse.getMessages()).haveAtLeastOne(new Condition<>(m -> {
-        try {
-          return m.getGMailLabels() != null && m.getGMailLabels().contains((GMailLabel) SystemLabel.DRAFTS);
-        } catch (UnfetchedFieldException e) {
-          return false;
-        }
-      }, "gmail labels"));
+      Optional<Set<GMailLabel>> allFetchedLabels = fetchResponse.getMessages()
+        .stream()
+        .map(m -> {
+          try {
+            return m.getGMailLabels();
+
+          } catch (UnfetchedFieldException e) {
+            throw new RuntimeException(e);
+          }
+        }).reduce(Sets::union);
+
+      assertThat(allFetchedLabels).isPresent();
+      assertThat(allFetchedLabels.get()).containsAll(systemLabels);
     }
   }
 }

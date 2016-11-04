2ce41e7a4f8994903638708ef6edd6dfbaa43b50
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2016-11-04 16:48:57 -0400

    Add other constructors

--- src/main/java/com/hubspot/imap/protocol/exceptions/AuthenticationFailedException.java
+++ src/main/java/com/hubspot/imap/protocol/exceptions/AuthenticationFailedException.java
@@ -11,6 +11,18 @@ public class AuthenticationFailedException extends Exception {
     super(message);
   }
 
+  public AuthenticationFailedException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public AuthenticationFailedException(Throwable cause) {
+    super(cause);
+  }
+
+  public AuthenticationFailedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
   public AuthenticationFailedException(String message, String extraData) {
     super(String.format("%s, Extra Data: '%s'", message, extraData));
   }

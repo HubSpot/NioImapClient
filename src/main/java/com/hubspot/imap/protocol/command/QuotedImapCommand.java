e301b0fd38b688bea35557ca474f3b214033f365
Ze'ev Klapow <zklapow@hubspot.com>
2017-08-22 15:37:23 -0400

    quote atoms in LOGIN

---
+++ src/main/java/com/hubspot/imap/protocol/command/QuotedImapCommand.java
@@ -0,0 +1,27 @@
+package com.hubspot.imap.protocol.command;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class QuotedImapCommand extends BaseImapCommand {
+  private static final String QUOTE = "\"";
+
+  public QuotedImapCommand(ImapCommandType type, String... args) {
+    super(type, args);
+  }
+
+  @Override
+  public List<String> getArgs() {
+    return super.getArgs().stream()
+        .map(QuotedImapCommand::quote)
+        .collect(Collectors.toList());
+  }
+
+  private static String quote(String in) {
+    StringBuilder result = new StringBuilder();
+    result.append(QUOTE);
+    result.append(in);
+    result.append(QUOTE);
+    return result.toString();
+  }
+}

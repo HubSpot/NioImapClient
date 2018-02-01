6a4725ca9f5104c24aa567546ce54d683078246f
Brian Cox <bcox@hubspot.com>
2018-02-01 14:56:42 -0500

    Added test

--- src/test/java/com/hubspot/imap/utils/parsers/EnvelopeParseTest.java
+++ src/test/java/com/hubspot/imap/utils/parsers/EnvelopeParseTest.java
@@ -4,7 +4,13 @@ import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
+import org.apache.james.mime4j.dom.Header;
+import org.apache.james.mime4j.dom.address.Mailbox;
+import org.apache.james.mime4j.field.Fields;
+import org.apache.james.mime4j.message.HeaderImpl;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.hubspot.imap.protocol.message.Envelope;
@@ -19,14 +25,29 @@ public class EnvelopeParseTest {
   private static final byte[] TEST_ENVELOPE = "(\"Tue, 28 Jul 2015 14:03:15 +0000\" \"Declined: weekly tennis @ Tue Jul 28, 2015 7pm - 9pm (jhuang@hubspot.com)\" ((\"Peter Casinelli\" NIL \"pcasinelli\" \"hubspot.com\")) ((\"Calendar <calendar-notification@google.com>\" NIL \"Google\" NIL)) ((\"Peter Casinelli\" NIL \"pcasinelli\" \"hubspot.com\")) ((NIL NIL \"jhuang\" \"hubspot.com\")) NIL NIL NIL \"<001a1135b6f27860f9051befeeb2@google.com>\"))\n".getBytes(StandardCharsets.UTF_8);
   private static final byte[] TEST_ENVELOPE_EMPTY_SUBJECT = "(\"Fri, 23 Jun 2017 17:55:45 +0200\" \"\" ((\"Test Name\" NIL \"testing0478\" \"gmail.com\")) ((\"Test Again\" NIL \"test12345\" \"gmail.com\")) ((\"Test Other\" NIL \"test456\" \"gmail.com\")) ((NIL NIL \"info\" \"test.com\")) NIL NIL NIL \"<testmessageid@mail.gmail.com>\")".getBytes(StandardCharsets.UTF_8);
 
+  private static final Mailbox ADDRESS1 = new Mailbox("bcox5021", "gmail.com");
+  private static final Mailbox ADDRESS2 = new Mailbox("bcox", "hubspot.com");
+  private static final Mailbox ADDRESS3 = new Mailbox("brian", "itscharlieb.com");
   private static final NestedArrayParser.Recycler<String> ARRAY_PARSER_RECYCLER = new Recycler<>(new LiteralStringParser(new SoftReferencedAppendableCharSequence(1000), 100000));
+
+  private Header header;
+
+  @Before
+  public void setup() {
+    header = new HeaderImpl();
+    header.addField(Fields.bcc());
+    header.addField(Fields.cc(ADDRESS2));
+    header.addField(Fields.to(ADDRESS3));
+    header.addField(Fields.date(Date.from(EnvelopeParser.parseDate("Mon, 29 Jan 2018 19:23:47 -0500").toInstant())));
+    header.addField(Fields.from(ADDRESS1));
+    header.addField(Fields.messageId("<CAKSL6jJTr_uwGqRDh4juaLLWdkmw0NyGcyiCYMFD15xm_A0=+w@mail.gmail.com>"));
+    header.addField(Fields.subject("Multiple two field"));
+  }
 
   @Test
   public void testCanParseEnvelope() throws Exception {
-    EnvelopeParser envelopeParser = new EnvelopeParser();
-
     NestedArrayParser<String> nestedArrayParser = ARRAY_PARSER_RECYCLER.get();
-    Envelope envelope = envelopeParser.parse(nestedArrayParser.parse(wrappedBuffer(TEST_ENVELOPE)));
+    Envelope envelope = EnvelopeParser.parse(nestedArrayParser.parse(wrappedBuffer(TEST_ENVELOPE)));
     nestedArrayParser.recycle();
 
     assertThat(envelope.getSubject()).isNotEmpty();
@@ -34,13 +55,18 @@ public class EnvelopeParseTest {
 
   @Test
   public void testCanParseEnvelope2() throws Exception {
-    EnvelopeParser envelopeParser = new EnvelopeParser();
-
     NestedArrayParser<String> nestedArrayParser = ARRAY_PARSER_RECYCLER.get();
-    Envelope envelope = envelopeParser.parse(nestedArrayParser.parse(wrappedBuffer(TEST_ENVELOPE_EMPTY_SUBJECT)));
+    Envelope envelope = EnvelopeParser.parse(nestedArrayParser.parse(wrappedBuffer(TEST_ENVELOPE_EMPTY_SUBJECT)));
     nestedArrayParser.recycle();
 
     assertThat(envelope.getSubject()).isEmpty();
     assertThat(envelope.getDateString()).isNotEmpty();
+  }
+
+  @Test
+  public void testCanParseHeader() throws Exception {
+    Envelope envelope = EnvelopeParser.parseHeader(header);
+    assertThat(envelope.getInReplyTo()).isNullOrEmpty();
+    assertThat(envelope.getMessageId()).isNotEmpty();
   }
 }

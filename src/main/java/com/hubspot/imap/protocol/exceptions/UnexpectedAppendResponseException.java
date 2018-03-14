768d170d93ec276f234244c7e6e0f9bc92e25659
Brian Cox <bcox@hubspot.com>
2018-03-14 17:29:26 -0400

    Response message in exception

--- src/main/java/com/hubspot/imap/protocol/exceptions/UnexpectedAppendResponseException.java
+++ src/main/java/com/hubspot/imap/protocol/exceptions/UnexpectedAppendResponseException.java
@@ -1,13 +1,19 @@
 package com.hubspot.imap.protocol.exceptions;
 
+import com.hubspot.imap.protocol.response.ImapResponse;
+
 public class UnexpectedAppendResponseException extends RuntimeException {
-  private static final String MESSAGE = "Received unexpected response from APPEND request";
+  private static final String MESSAGE = "Received unexpected response from APPEND request: %s";
 
   public UnexpectedAppendResponseException() {
     super(MESSAGE);
   }
 
+  public UnexpectedAppendResponseException(ImapResponse response) {
+     super(String.format(MESSAGE, response.getMessage()));
+  }
+
   public UnexpectedAppendResponseException(Throwable throwable) {
-    super(MESSAGE, throwable);
+    super(String.format(MESSAGE, throwable.getMessage()), throwable);
   }
 }

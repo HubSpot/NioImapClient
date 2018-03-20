9aa203338c49d206ca0aed675c9ea64132f4e73d
Charlie Bloomfield <charlielbloomfield@gmail.com>
2018-03-20 16:43:34 -0400

    String Literal Support (#53)
    * cleanup pr
    
    * no memeory
    
    * pr review
    
    * nit
    
    * remove test
    
    * review and nit

--- src/main/java/com/hubspot/imap/utils/parsers/string/AtomOrStringParser.java
+++ src/main/java/com/hubspot/imap/utils/parsers/string/AtomOrStringParser.java
@@ -1,9 +1,12 @@
 package com.hubspot.imap.utils.parsers.string;
 
+import java.nio.charset.StandardCharsets;
+
 import com.hubspot.imap.utils.SoftReferencedAppendableCharSequence;
 import com.hubspot.imap.utils.parsers.ByteBufParser;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.DecoderException;
 import io.netty.handler.codec.TooLongFrameException;
 import io.netty.util.internal.AppendableCharSequence;
 
@@ -52,6 +55,8 @@ public class AtomOrStringParser implements ByteBufParser<String> {
       } else if (!isQuoted && (c == ')' || c == '(')) {
         buffer.readerIndex(buffer.readerIndex() - 1);
         break;
+      } else if (!isQuoted && (c == '{')) {
+        return parseLiteral(buffer, seq);
       } else {
         append(seq, c);
       }
@@ -67,6 +72,53 @@ public class AtomOrStringParser implements ByteBufParser<String> {
     return seq.toString();
   }
 
+  /**
+   * See https://tools.ietf.org/html/rfc3501.html#section-4.3
+   *
+   * String literals have the form
+   *
+   * {10}
+   * abcdefghij
+   *
+   * Where {10} represents the length of the string literal. The literal
+   * begins after any CLRF characters following the '}' character.
+   */
+  private String parseLiteral(ByteBuf buffer, AppendableCharSequence seq) {
+    int length = 0;
+
+    char c = (char) buffer.readUnsignedByte();
+    while (c != '}') {
+      if (Character.isDigit(c)) {
+        int digit = Character.digit(c, 10);
+        length = (length * 10) + digit;
+        c = (char) buffer.readUnsignedByte();
+      } else {
+        throw new DecoderException(
+            String.format("Found non-digit character %c where a digit was expected", c)
+        );
+      }
+    }
+
+    if (length > 0) {
+      //ignore crlf characters after '}'
+      c = (char) buffer.readUnsignedByte();
+      while (isCRLFCharacter(c)) {
+        c = (char) buffer.readUnsignedByte();
+      }
+
+      append(seq, c);
+      append(seq, buffer, length);
+
+      return seq.toString();
+    } else {
+      return "";
+    }
+  }
+
+  private boolean isCRLFCharacter(char c) {
+    return c == '\n' || c == '\r';
+  }
+
   private void append(AppendableCharSequence seq, char c) {
     if (size >= maxStringLength) {
       throw new TooLongFrameException("String is larger than " + maxStringLength + " bytes.");
@@ -76,4 +128,12 @@ public class AtomOrStringParser implements ByteBufParser<String> {
     seq.append(c);
   }
 
+  private void append(AppendableCharSequence seq, ByteBuf buffer, int length) {
+    if (size + length >= maxStringLength) {
+      throw new TooLongFrameException("String is larger than " + maxStringLength + " bytes.");
+    }
+
+    size += length;
+    seq.append(buffer.readCharSequence(length - 1, StandardCharsets.UTF_8));
+  }
 }

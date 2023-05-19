80e809d71f679d55fbd9b9fa3d3fdecb809f52af
Ben Ross <bross@hubspot.com>
2023-05-19 15:36:35 -0400

    Add ability to log socks proxy address

---
+++ src/main/java/com/hubspot/imap/utils/SocksProxyLogger.java
@@ -0,0 +1,27 @@
+package com.hubspot.imap.utils;
+
+
+import java.net.InetSocketAddress;
+
+import org.slf4j.Logger;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.proxy.Socks4ProxyHandler;
+
+public class SocksProxyLogger extends ChannelInboundHandlerAdapter {
+  private Logger logger;
+
+  public SocksProxyLogger(String clientName) {
+    this.logger = LogUtils.loggerWithName(SocksProxyLogger.class, clientName);
+  }
+
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    super.channelActive(ctx);
+    Socks4ProxyHandler proxyHandler = ctx.channel().pipeline().get(Socks4ProxyHandler.class);
+    InetSocketAddress proxyAddress = proxyHandler.proxyAddress();
+    String hostAddress = proxyAddress.getAddress().getHostAddress();
+    logger.info("Using SOCKS proxy address {}", hostAddress);
+  }
+}

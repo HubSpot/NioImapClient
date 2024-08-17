ff0173dc1fe00184d42298f5e4bf29df6c887332
Yongbo Niu <yniu@hubspot.com>
2024-08-16 23:44:49 -0400

    add unit test

--- src/test/java/com/hubspot/imap/StreamingFetchCommandTest.java
+++ src/test/java/com/hubspot/imap/StreamingFetchCommandTest.java
@@ -11,11 +11,7 @@ import com.hubspot.imap.protocol.response.tagged.StreamingFetchResponse;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,21 +30,53 @@ public class StreamingFetchCommandTest extends BaseGreenMailServerTest {
   public void initialize() throws Exception {
     super.setUp();
     client = getLoggedInClient();
-    deliverRandomMessage();
+  }
+
+  @Test
+  public void testOnFetchWithOnlyStartUid_ItCallsBackStreamingMethod() throws Exception {
+    deliverRandomMessages(1);
     CompletableFuture<OpenResponse> openFuture = client.open(
       DEFAULT_FOLDER,
       FolderOpenMode.READ
     );
     uidNext = openFuture.join().getUidNext();
-  }
-
-  @Test
-  public void testOnFetch_ItCallsBackStreamingMethod() throws Exception {
     CountDownLatch countDownLatch = new CountDownLatch(1);
 
+    // per rfc 3501,
+    // "Also note that a UID range of 559:* always includes the
+    // UID of the last message in the mailbox, even if 559 is
+    // higher than any assigned UID value."
     CompletableFuture<StreamingFetchResponse<Void>> fetchFuture = client.uidfetch(
       uidNext,
       Optional.empty(),
+      imapMessage -> {
+        countDownLatch.countDown();
+        return null;
+      },
+      FetchDataItemType.ENVELOPE
+    );
+    fetchFuture.join();
+    assertThat(fetchFuture.isDone()).isTrue();
+    assertThat(fetchFuture.isCompletedExceptionally()).isFalse();
+    assertThat(fetchFuture.get().getCode()).isEqualTo(ResponseCode.OK);
+
+    assertThat(countDownLatch.await(1, TimeUnit.SECONDS)).isTrue();
+  }
+
+  @Test
+  public void testOnFetchWithUidRange_ItCallsBackStreamingMethod() throws Exception {
+    int messageCount = 3;
+    deliverRandomMessages(messageCount);
+    CompletableFuture<OpenResponse> openFuture = client.open(
+      DEFAULT_FOLDER,
+      FolderOpenMode.READ
+    );
+    uidNext = openFuture.join().getUidNext();
+    CountDownLatch countDownLatch = new CountDownLatch(messageCount);
+
+    CompletableFuture<StreamingFetchResponse<Void>> fetchFuture = client.uidfetch(
+      1,
+      Optional.of(uidNext),
       imapMessage -> {
         countDownLatch.countDown();
         return null;
